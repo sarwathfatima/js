35267ee2698eae2e1bcfd17fa0ebbd15944f7c40
sarwathfatima <syedasfqadri98@gmail.com>
2022-11-23 15:31:32 +0530

    i push a file

---
+++ javascript/learn.js
@@ -0,0 +1,15 @@
+console.log("helloworld");
+console.log("wizarath","sarwath");
+console.log("welcom to 'js'");
+console.log('adnan "ali" khan');
+console.log("5/10=0.5");
+console.log("this is 'js'");
+console.log("hi. how are you");
+console.log(":\\\\\\");
+console.log("adnan \"ali\" khan")
+
+
+
+var a = 8
+var b = 9
+n
